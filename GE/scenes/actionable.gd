f5c7c33a7c3eaa4d345e0600ad053f476ec662e2
StorytellerIgneel <teohwh2004@gmail.com>
2024-07-18 03:03:17 +0800

    dialogue system developing

---
+++ GE/scenes/actionable.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func action() -> void:
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
