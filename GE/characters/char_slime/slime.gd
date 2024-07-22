41eace0a5f8da7fa853d17757949a5ecf76e6d71
domo536 <domo_536@yahoo.com.my>
2024-07-23 01:57:39 +0800

    Soldier V2 to interact with enemy

--- GE/characters/char_slime/slime.gd
+++ GE/characters/char_slime/slime.gd
@@ -7,9 +7,6 @@ var player = null
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
-		
-		
-
 
 func _on_area_2d_body_entered(body):
 	player = body
@@ -19,16 +16,3 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	player = null
 	player_chase = false
-
-
-
-
-
-#func _on_area_2d_area_entered(area):
-	#player = area
-	#player_chase = true
-#
-#
-#func _on_area_2d_area_exited(area):
-	#player = null
-	#player_chase = false
