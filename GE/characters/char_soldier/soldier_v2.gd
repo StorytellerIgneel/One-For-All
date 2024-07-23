44a3d051e1f9fc67f15d1b9d894cb4fa2b0739ad
domo536 <domo_536@yahoo.com.my>
2024-07-24 01:47:00 +0800

    Added attack module for Soldier and Slime for testing =D

--- GE/characters/char_soldier/soldier_v2.gd
+++ GE/characters/char_soldier/soldier_v2.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+var enemy_in_atk_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+var attack_ip = false
+
 @onready var _anim = $AnimatedSprite2D
 @onready var actionable_finder = $Direction/ActionableFinder
 
@@ -19,6 +26,14 @@ func _physics_process(delta):
 		player_movement(delta)
 	mc_animate()
 	check_interact()
+	enemy_attack()
+	attack()
+	
+	if health <= 0:
+		player_alive = false  # add end screen
+		health = 0
+		print("player has been killed")
+		self.queue_free()
 	pass
 
 func check_interact():
@@ -54,17 +69,65 @@ func player_movement(delta):
 	pass
 
 func mc_animate():
-	if Input.is_action_pressed("toLeft"):
-		if $AnimatedSprite2D.flip_h == false:
-			$AnimatedSprite2D.flip_h = true
-		_anim.play("soldier_walk")
+	if Input.is_action_pressed("atk1"):
+		_anim.play("soldier_atk1")
+	elif Input.is_action_pressed("atk2"):
+		_anim.play("soldier_atk2")
+	elif Input.is_action_pressed("atk3"):
+		_anim.play("soldier_atk3")
+	else:
+		if Input.is_action_pressed("toLeft"):
+			if $AnimatedSprite2D.flip_h == false:
+				$AnimatedSprite2D.flip_h = true
+			_anim.play("soldier_walk")
+			pass
+		elif Input.is_action_pressed("toRight"):
+			if $AnimatedSprite2D.flip_h == true:
+				$AnimatedSprite2D.flip_h = false
+			_anim.play("soldier_walk")
+			pass
+		elif attack_ip == false || Input.is_action_just_released("toLeft") || Input.is_action_just_released("toRight"):
+			_anim.play("soldier_idle")
 		pass
-	elif Input.is_action_pressed("toRight"):
-		if $AnimatedSprite2D.flip_h == true:
-			$AnimatedSprite2D.flip_h = false
-		_anim.play("soldier_walk")
-		pass
-	elif Input.is_action_just_released("toLeft") || Input.is_action_just_released("toRight"):
-		_anim.play("soldier_idle")
 	pass
-pass
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_in_atk_range = true
+
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_in_atk_range = false
+
+func enemy_attack():
+	if enemy_in_atk_range and enemy_attack_cooldown ==true:
+		health = health - 5
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print("player health = ", health)
+
+
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+
+func attack():
+	#var dir = current_dir
+	if Input.is_action_just_pressed("atk1"):
+		Global.player_current_attack = true
+		attack_ip = true
+		if $AnimatedSprite2D.flip_h == false:
+			$AnimatedSprite2D.play("soldier_atk1")
+			$deal_attack_timer.start()
+		if $AnimatedSprite2D.flip_h == true:
+			$AnimatedSprite2D.play("soldier_atk1")
+			$deal_attack_timer.start()
+
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	Global.player_current_attack = false
+	attack_ip = false
