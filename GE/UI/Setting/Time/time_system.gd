e801b096c974248e5490e6c2d35db693f2a6fca0
Darkridersia <riderdark795@gmail.com>
2024-07-23 12:25:47 +0800

    Added Time

---
+++ GE/UI/Setting/Time/time_system.gd
@@ -0,0 +1,13 @@
+class_name TimeSystem extends Node
+
+signal updated
+
+@export var date_time: DateTime
+# Set Ticks using the system can be found in Test.tscn and in TimeSystem node
+@export var ticks_pr_second: int = 6
+
+func _process(delta: float) -> void:
+	date_time.increase_by_sec(delta * ticks_pr_second)
+	updated.emit(date_time)
+
+	
