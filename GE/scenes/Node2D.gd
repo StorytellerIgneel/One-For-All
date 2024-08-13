24f282716bd58616223616af2dcdc4f84f49b405
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-08-14 01:53:07 +0800

    Update Node2D.gd

--- GE/scenes/Node2D.gd
+++ GE/scenes/Node2D.gd
@@ -11,27 +11,27 @@ func _ready():
 
 	# Create a viewport and set its size to the size of the TileMap
 	var viewport := $Viewport
-	var map_size := tilemap.get_used_rect().size * tilemap.cell_size
-	viewport.size = map_size
-	viewport.clear_mode = Viewport.CLEAR_MODE_ONLY_NEXT_FRAME
-	viewport.render_target_update_mode = Viewport.UPDATE_ONCE
+	#var map_size := tilemap.get_used_rect().size * tilemap.cell_size
+	#viewport.size = map_size
+	#viewport.clear_mode = Viewport.CLEAR_MODE_ONLY_NEXT_FRAME
+	#viewport.render_target_update_mode = Viewport.UPDATE_ONCE
 
 	# Instance the TileMap and add it to the Viewport's scene
-	var tilemap_instance := tilemap.duplicate()
-	tilemap_parent.add_child(tilemap_instance)
-	viewport.add_child(tilemap_parent)
+	#var tilemap_instance := tilemap.duplicate()
+	#tilemap_parent.add_child(tilemap_instance)
+	#viewport.add_child(tilemap_parent)
 
 	# Render one frame to update the Viewport texture
-	viewport.render_target_update_mode = Viewport.UPDATE_ONCE
-w	yield(viewport, "size_changed")
+	#viewport.render_target_update_mode = Viewport.UPDATE_ONCE
+	#yield(viewport, "size_changedd")
 
 	# Create an image from the viewport
-	var image := viewport.get_texture().get_image()
-	image.flip_y()  # Flip the image if necessary
+	#var image := viewport.get_texture().get_image()
+	#image.flip_y()  # Flip the image if necessary
 
 	# Save the image as a PNG file
-	var error := image.save_png(output_file_path)
-	if error == OK:
-		print("TileMap exported successfully.")
-	else:
-		print("Error exporting TileMap.")
+	#var error := image.save_png(output_file_path)
+	#if error == OK:
+	#	print("TileMap exported successfully.")
+	#else:
+	#	print("Error exporting TileMap.")
