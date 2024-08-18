b51402c6b5270ed32e4b6017ebb6ff1470f6edb2
Sia Li Ze <riderdark795@gmail.com>
2024-08-18 17:31:18 +0800

    Testing adding to new branch

---
+++ GE/UI/HUD/Skill/ActionBar.gd
@@ -0,0 +1,14 @@
+extends HBoxContainer
+
+# Use Array to store btns
+var skills : Array
+@onready var debug = $"../debug"
+ 
+func _ready():
+	skills = get_children()
+	for i in get_child_count():
+		skills[i].change_key = str(i+1)
+ 
+ 
+func _casted(spell_name):
+	debug.text = spell_name
