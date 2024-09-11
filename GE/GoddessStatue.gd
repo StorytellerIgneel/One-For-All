0afc12f13f7d3326379e8155ce86614505dfa2b6
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-09-12 00:28:15 +0800

    goddess statue done without dialogue

--- GE/GoddessStatue.gd
+++ GE/GoddessStatue.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ()
-	pass
+	var playerDetected = ($"Goddess Statue/HealingArea".get_overlapping_areas());
+	
+	if (playerDetected.size() > 0): #player detected
+		
+		if (playerDetected[0].name == "ActionableFinder"):
+			playerDetected[0].get_parent().get_parent().health = 100
--- GE/UI/Setting/Time/date_time.gd
+++ GE/UI/Setting/Time/date_time.gd
@@ -25,5 +25,5 @@ func increase_by_sec(delta_seconds: float) -> void:
 	minutes = minutes % 60
 	hours = hours % 24
 	
-	print_debug((str(days) + ":" + str(hours) + ":" + str(minutes) + ":" + str(seconds)))
+	#print_debug((str(days) + ":" + str(hours) + ":" + str(minutes) + ":" + str(seconds)))
 	
