b142b7348c14876bac7e37b291a388c62df6a0bd
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-09-14 11:33:57 +0800

    update

--- GE/ChestArea.gd
+++ GE/ChestArea.gd
@@ -8,9 +8,9 @@ func _physics_process(delta):
 func action():
 	var chestOpen = Image.load_from_file("res://resources/tilemap/plain/Texture/openedChest.png")
 	self.get_parent().get_node("Sprite2D").texture = ImageTexture.create_from_image(chestOpen)
-	if get_parent().has_Key == true and Global.findingKey == 3:
+	if get_parent().hasKey == true and Global.findingKey == 3:
 		Global.trigger_dialogue("res://Dialogues/Key3.dialogue", "start")
-	elif get_parent().has_Key == true and Global.findingKey != 3:
+	elif get_parent().hasKey == true and Global.findingKey != 3:
 		Global.trigger_dialogue("res://Dialogues/wrongKey.dialogue", "start")
 	else: #no key
 		Global.trigger_dialogue("res://Dialogues/PlainItemsNoKey.dialogue", "start")
