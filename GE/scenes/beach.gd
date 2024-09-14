bda4276d287dd7f62b007d6408eb94aa961178b6
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-09-14 14:56:52 +0800

    clean code

--- GE/scenes/beach.gd
+++ GE/scenes/beach.gd
@@ -1,7 +1,5 @@
 extends Node
 
-var NextLevel: bool = false
-
 @onready var pause_menu = $CanvasLayer/InputSettings
 
 var game_paused = false
@@ -17,7 +15,7 @@ func _ready():
 	# Load the dialogue resource directly from the path
 	player.set_muddy_region(muddyRegion)
 	
-	#Global.trigger_dialogue("res://Dialogues/beach.dialogue", "beach_start")
+	Global.trigger_dialogue("res://Dialogues/beach.dialogue", "beach_start")
 
 	initialize_camera_limit()
 	
@@ -42,11 +40,6 @@ func _unhandled_input(event):
 			pause_menu.visible = false
 			
 		get_tree().root.get_viewport().set_input_as_handled()
-	
-	if event.is_action_pressed("NextMap"):
-		await LoadManager.load_scene("NextMap")
-		
-		get_tree().change_scene_to_file("res://plain.tscn")
 		
 func initialize_camera_limit():
 	$soldierV2/PlayerCamera.limit_right = $BeachTileMap.get_used_rect().size.x * 16
--- GE/scenes/houseInteract.gd
+++ GE/scenes/houseInteract.gd
@@ -1,18 +1,12 @@
 extends Area2D
 
-const balloon = preload("res://Dialogues/balloon.tscn")
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func action() -> void:
+func action():
 	#DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
-	var balloon: Node = balloon.instantiate()
-	get_tree().current_scene.add_child(balloon)
-	balloon.start(dialogue_resource, dialogue_start)
+	Global.trigger_dialogue("res://Dialogues/house.dialogue", "start")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
