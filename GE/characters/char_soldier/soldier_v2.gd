b9aa15611ac3864e7394f34a416ede9b36178995
Sia Li Ze <riderdark795@gmail.com>
2024-09-15 20:36:30 +0800

    Syncing

--- GE/characters/char_soldier/soldier_v2.gd
+++ GE/characters/char_soldier/soldier_v2.gd
@@ -93,7 +93,7 @@ func check_interact():
 		if actionables.size() > 1: #always more than one since overlap with hitbox
 			print(actionables)
 			var actionable = actionables[1]
-			actionable.action()
+			#actionable.action()
 	
 	if Input.is_action_just_pressed("createTile"):
 		place_tile_in_front()
