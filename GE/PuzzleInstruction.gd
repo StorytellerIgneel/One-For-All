d55e7d217fe022bdd4ff86a8c15b081e4bbdedd4
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-09-18 22:24:06 +0800

    puzzle instruction settle

--- GE/PuzzleInstruction.gd
+++ GE/PuzzleInstruction.gd
@@ -4,16 +4,17 @@ var currentMap
 var triggerDialogue
 
 func _ready():
-	if (currentMap == "BeachTileMap"):
-		triggerDialogue = "beach"
-	elif (currentMap == "PlainTileMap"):
-		triggerDialogue = "plain"
-	elif (currentMap == "WinterfellTileMap"):
-		triggerDialogue = "winterfell"
-	elif (currentMap == "VolcanoTileMap"):
-		triggerDialogue = "volcano"
+	print(currentMap)
 	
 func action():
 	currentMap = Global.currentTilemap.name
+	if (currentMap == "BeachTileMap"):
+		triggerDialogue = "res://Dialogues/beachPuzzle.dialogue"
+	elif (currentMap == "PlainTileMap"):
+		triggerDialogue = "res://Dialogues/plainPuzzle.dialogue"
+	elif (currentMap == "WinterfellTileMap"):
+		triggerDialogue = "res://Dialogues/winterfellPuzzle.dialogue"
+	elif (currentMap == "VolcanoTileMap"):
+		triggerDialogue = "res://Dialogues/volcanoPuzzle.dialogue"
 	
-	Global.trigger_dialogue("res://instructions.dialogue", triggerDialogue)
+	Global.trigger_dialogue(triggerDialogue, "start")
