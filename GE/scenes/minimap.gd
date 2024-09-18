4b45d4a03fade626783a51b736d264cc8bace34a
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2024-09-18 23:59:02 +0800

    minimap done

--- GE/scenes/minimap.gd
+++ GE/scenes/minimap.gd
@@ -2,6 +2,7 @@ extends SubViewport
 
 @onready var camera = $Camera2D
 @onready var player = get_node("../../../soldierV2")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,5 +10,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	camera.position = player.position
+	camera.position = player.position
 	pass
